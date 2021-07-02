0699874c659ed6619d77eb229cbc9aa0c8e28914
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-02 01:26:24 +0100

    added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main class="home">
+    
+  </main>
 </template>
 
 <script>
@@ -9,6 +9,31 @@
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data () {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/Spinner-3.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created (){
+    const data = await this.fetchCovidData()
+    
+    this.dateDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
 }
 </script>
