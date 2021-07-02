bc554778f0b78c971bfb500e9303cf6e8177bc60
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-02 23:43:26 +0100

    added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,12 +14,14 @@
 <script>
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
-    DataBoxes
+    DataBoxes,
+    CountrySelect
   },
   data () {
     return {
