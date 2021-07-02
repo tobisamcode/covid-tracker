d09896a3369d0de427623d0be8ef097f800f7e96
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-02 01:52:50 +0100

    added

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="text-center">
         <h2 class="text-3xl font-bold">{{text}}</h2>
-        <div class="mt-4 mb-10 text-2xl">{{dataDate}}</div>
+        <div class="mt-4 mb-10 text-2xl">{{timestamp}}</div>
     </div>
 
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
     name: 'DataTitle',
-    props: ['text', 'dataDate']
+    props: ['text', 'dataDate'],
+    computed: {
+        timestamp: function () {
+            return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+        }
+    },
 }
 </script>
