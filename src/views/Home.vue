a9e110c244d67cba640f04a5b3df32f51ea01fc5
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-05 02:27:52 +0100

    added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
 
     <DataBoxes :stats="stats"/>
 
-    <CountrySelect :countries="countries"/>
+    <CountrySelect @get-country="getCountryData" :countries="countries"/>
   </main>
 
   <main class="flex flex-col justify-center text-center align-center" v-else>
@@ -40,6 +40,11 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+
+    getCountryData(country){
+      this.stats = country
+      this.title = country.Country
     }
   },
   async created (){
