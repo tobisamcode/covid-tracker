9abf442cd535e2d1e782a6bb9f038317ffef0c1d
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-05 02:13:26 +0100

    added

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,12 +1,23 @@
 <template>
+    <select @change="onChange()" 
+        v-model="selected" class="block w-full p-3 mt-10 border rounded form-select ">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.ID">
+            {{country.Country}}
+        </option>
+    </select>
+        
     
 </template>
-
 
 <script>
 export default {
     name: 'CountrySelect',
-    props: ['countries']
-
+    props: ['countries'],
+    data(){
+        return{
+            selected: 0
+        }
+    }
 }
 </script>
